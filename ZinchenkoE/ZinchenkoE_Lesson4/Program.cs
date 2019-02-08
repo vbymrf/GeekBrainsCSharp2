e819e71ea0786185e2b2b2269b99e6b50d241606
Evgeny <vbymrf@bk.ru>
2019-02-08 16:26:00 +0300

    Не выполнено

--- ZinchenkoE/ZinchenkoE_Lesson4/Program.cs
+++ ZinchenkoE/ZinchenkoE_Lesson4/Program.cs
@@ -11,10 +11,10 @@ namespace ZinchenkoE_Lesson4
         static void Main(string[] args)
         {
            int[] lis = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
-//​var​ posNums ​=​ ​from​ n ​in​ lis 
-//                      ​where​ n ​>​ ​0
-//                      ​where​ n ​<​ ​10
-//                      ​select​ n;
+​var​ posNums ​=​ ​from​ n ​in​ lis 
+                      ​where​ n ​>​ ​0
+                      ​where​ n ​<​ ​10
+                      ​select​ n;
 
 
             Console.WriteLine(typeof(int));
