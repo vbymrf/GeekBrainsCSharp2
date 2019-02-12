1faaff2f4f4164d3fdc416d68ab29679e5543548
Evgeny <vbymrf@bk.ru>
2019-02-12 16:51:05 +0300

    Выполнил логическую часть ДЗ (1) Дизайн не создан. Не знаю как привязять логику.

---
+++ ZinchenkoE/ZinchenkoE_Lesson6/​Department.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZinchenkoE_Lesson6
+{
+    class Department
+    {
+        string Name;
+        public static int Number = 0;
+        List<Employee> Data;
+
+        public static List<Department> DepartmentList=new List<Department>();
+
+        public Department()
+        {
+            Number++;
+            this.Name = "Departament" + Number;            
+            //this.Data.Add(null);
+        }
+        public Department(int kol)
+        {
+            Number++;
+            this.Name = "Departament" + Number;
+            for (int i = 0; i < kol; i++)
+            {
+            Employee ee = new Employee();
+            this.Data.Add(ee);
+            ee.depEmployee = this;
+            } 
+        }
+        public Department ChangeDepartment (string Name)
+        {
+            this.Name = Name;
+            return this;
+        }
+        public void AddEmployee(Employee e)
+        {
+            foreach(Employee em in this.Data)
+            {
+               if( e.Equals(e)) return;
+            }
+            Data.Add(e);
+            e.depEmployee = this;
+        }
+        public void DeletEmployee(Employee e)
+        {
+            Data.Remove(e);
+            e.depEmployee = null;
+        }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+
+    }
+}
---
+++ ZinchenkoE/ZinchenkoE_Lesson6/​Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZinchenkoE_Lesson6
+{
+    class Employee
+    {
+        string Name;
+        string SurName;
+        string Professian;
+        public  Department depEmployee;
+
+        public Employee()
+        {
+            Random ran = new Random();
+            this.Name = "Name" + ran.Next();
+            this.SurName = "SurName" + ran.Next();
+            this.Professian = "Professian" + ran.Next();
+            this.depEmployee=null;
+        }
+
+        public Employee ChangeEmployee(Employee e,string Name, string SurName, string Professian)
+        {
+            if (e != null)
+            {
+                if (Name != null && Name != "0") this.Name = Name;
+                if (SurName != null && SurName != "0") this.SurName = SurName;
+                if (Professian != null && Professian != "0") this.Professian = Professian;
+            }
+            return e;
+        }
+        public void ChangeDeportament(Employee e, Department d)
+        {
+            d.DeletEmployee(e);
+            d.AddEmployee(e);
+        }
+    }
+}
